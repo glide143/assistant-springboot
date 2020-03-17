692ae4e5f0b7b8a10c7331aa8008f713d5403885
Melchizedek <melchizedek30@gmail.com>
2020-03-17 17:10:49 +0800

    added text

--- src/main/java/com/my/test/app/service/MyService.java
+++ src/main/java/com/my/test/app/service/MyService.java
@@ -25,24 +25,28 @@ public class MyService extends DialogflowApp {
         ResponseBuilder responseBuilder = getResponseBuilder(request);
         responseBuilder.add("Welcome to my app");
 
-        List<Button> buttons = Collections.singletonList(new Button().setTitle("Click HERE").setOpenUrlAction(new OpenUrlAction().setUrl("https://www.google.com/")));
+        List<Button> buttons = Collections.singletonList(new Button().setTitle("Click HERE")
+                                                                     .setOpenUrlAction(new OpenUrlAction().setUrl(
+                                                                             "https://www.google.com/")));
         List<String> suggestions = Arrays.asList("Hey you", "Whats up");
 
         Image image = new Image();
         image.setUrl(IMG_URL);
         image.setAccessibilityText("Image alt text");
 
+        String text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 
         BasicCard basicCard = new BasicCard();
         basicCard.setTitle("TThis is a title");
         basicCard.setSubtitle("This is a subtitle");
         basicCard.setImage(image);
+        basicCard.setFormattedText(text);
         basicCard.setButtons(buttons);
 
         responseBuilder.add("This is the first simple response for a basic card.")
                        .add(basicCard)
-                       .addSuggestions(suggestions.toArray(new String[]{}));
-        
+                       .addSuggestions(suggestions.toArray(new String[] {}));
+
         return responseBuilder.build();
     }
 }
