a0dc5b9c76becbdea74c723210910342620e7c58
Melchizedek <melchizedek30@gmail.com>
2020-03-18 12:18:30 +0800

    Refactored code.

--- src/main/java/com/my/test/app/service/MyService.java
+++ src/main/java/com/my/test/app/service/MyService.java
@@ -36,14 +36,13 @@ public class MyService extends DialogflowApp {
 
         String text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 
-        BasicCard basicCard = new BasicCard();
-        basicCard.setTitle("TThis is a title");
-        basicCard.setSubtitle("This is a subtitle");
-        basicCard.setImage(image);
-        basicCard.setFormattedText(text);
-        basicCard.setButtons(buttons);
+        BasicCard basicCard = new BasicCard().setTitle("Lorem Ipsum")
+                                             .setSubtitle("Lorem Ipsum")
+                                             .setImage(image)
+                                             .setFormattedText(text)
+                                             .setButtons(buttons);
 
-        responseBuilder.add("This is the first simple response for a basic card.")
+        responseBuilder.add(text)
                        .add(basicCard)
                        .addSuggestions(suggestions.toArray(new String[] {}));
 
