8e0f08b366a5a776cce121f88e3d3c80ab351a28
Melchizedek <melchizedek30@gmail.com>
2020-03-18 14:56:03 +0800

    test7

--- src/main/java/com/my/test/app/service/MyService.java
+++ src/main/java/com/my/test/app/service/MyService.java
@@ -7,7 +7,6 @@ import com.google.actions.api.ForIntent;
 import com.google.actions.api.response.ResponseBuilder;
 import com.google.api.services.actions_fulfillment.v2.model.*;
 import com.my.test.app.domain.FunFact;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -31,30 +30,11 @@ public class MyService extends DialogflowApp {
         List<Button> buttons = Collections.singletonList(new Button().setTitle("Click HERE")
                                                                      .setOpenUrlAction(new OpenUrlAction().setUrl(
                                                                              "https://www.google.com/")));
-        List<String> suggestions = Arrays.asList("Hey you", "Whats up");
+        List<String> suggestions = Arrays.asList("Hey you", "Hi");
 
         FunFact funFact = funFactService.getRandomFunFact();
 
-        Image image = new Image();
-        image.setUrl(funFact.getUrlImg());
-        image.setAccessibilityText("Image alt text");
-
-        //String text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
-
-        BasicCard basicCard = createBasicCard(buttons, image, funFact.getMsg());
-
-        //        MediaObject mediaObject = new MediaObject().setName("SoundHelix-Song-1")
-        //                                                   .setContentUrl(
-        //                                                           "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3")
-        //                                                   .setIcon(image);
-        //
-        //        RichResponse richResponse = new RichResponse();
-        ////
-        ////        .add(basicCard)
-        ////                .add(new SimpleResponse().setTextToSpeech(text))
-        //
-        //        responseBuilder.add(new MediaResponse().setMediaObjects(Collections.singletonList(mediaObject)).setMediaType("AUDIO"))
-        //                       .addSuggestions(suggestions.toArray(new String[] {}));
+        BasicCard basicCard = createFunFactBasicCard(buttons,funFact);
 
         responseBuilder.add(basicCard)
                        .addSuggestions(suggestions.toArray(new String[] {}));
@@ -62,15 +42,15 @@ public class MyService extends DialogflowApp {
         return responseBuilder.build();
     }
 
-    private BasicCard createBasicCard(List<Button> buttons, Image image, String text) {
+    private BasicCard createFunFactBasicCard(List<Button> buttons, FunFact funFact) {
+        Image image = new Image();
+        image.setUrl(funFact.getUrlImg());
+        image.setAccessibilityText("Image alt text");
 
-
-
-
-        return new BasicCard().setTitle("Lorem Ipsum")
-                              .setSubtitle("Lorem Ipsum")
+        return new BasicCard().setTitle(funFact.getTitle())
+                              .setSubtitle(funFact.getSubTitle())
                               .setImage(image)
-                              .setFormattedText(text)
+                              .setFormattedText(funFact.getMsg())
                               .setButtons(buttons);
     }
 }
